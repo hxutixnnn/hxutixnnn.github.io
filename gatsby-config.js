edccf5b41384924816d44a177da5b1bb0bbba9af
hxutixnnn <ng.huutien98@gmail.com>
2023-01-11 14:18:36 +0700

    feat: update all contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
-const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+const googleAnalyticsTrackingId = process.env.GOOGLE_ANALYTICS_ID;
 
 module.exports = {
-  pathPrefix: '/',
+  pathPrefix: "/",
   siteMetadata: {
-    siteTitle: `Huu-Tien Nguyen`,
-    siteTitleAlt: `Huu-Tien Nguyen - My Personal Blog`,
-    siteHeadline: `Huu-Tien Nguyen - Software Developer`,
-    siteUrl: `https://hxutixnnn.github.io`,
-    siteDescription: `My Personal Blog`,
-    siteLanguage: `vi`,
+    siteTitle: `Nguyễn Hữu Tiền`,
+    siteTitleAlt: `Tien Nguyen - My Personal Website`,
+    siteHeadline: `Tien's Personal Website`,
+    siteUrl: `https://nguyenhuutien.com`,
+    siteDescription: `Hi, my name is Tien. Here, I will write about things I am learning, learned, and will learn in the future.`,
+    siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@hxutixnnn`,
   },
@@ -107,8 +107,8 @@ module.exports = {
           {
             serialize: ({ query: { site, allPost } }) =>
               allPost.nodes.map((post) => {
-                const url = site.siteMetadata.siteUrl + post.slug
-                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+                const url = site.siteMetadata.siteUrl + post.slug;
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`;
 
                 return {
                   title: post.title,
@@ -117,7 +117,7 @@ module.exports = {
                   url,
                   guid: url,
                   custom_elements: [{ "content:encoded": content }],
-                }
+                };
               }),
             query: `
               {
@@ -146,4 +146,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
